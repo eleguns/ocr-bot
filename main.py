140d263a162bd7874e9b2fe7eccc905d59eb0d53
eleguns <d88560771@gmail.com>
2023-07-04 23:12:54 +0330

    completing bot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from PIL import Image
 import config
+from pytesseract import image_to_string as convert
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
@@ -16,7 +18,17 @@ async def start(c: Client, m:Message):
 
 @app.on_message(filters.private & filters.photo)
 async def extraction(c: Client, m: Message):
-    pass
+    try :
+        file = await c.download_media(m, in_memory=True)
+    except Exception as e :
+        print(f'error : {e}')
+        await m.reply('can\'t download file')
+    buffer = file.getbuffer()
+    try :
+        text = convert(Image.open(buffer))
+    except Exception as e :
+        print(f'error : {e}')
+        await m.reply('an error occurred, please try again or report to the admin')
 
 @app.on_message(filters.group)
 async def join(c: Client, m: Message):
@@ -24,3 +36,17 @@ async def join(c: Client, m: Message):
     me_id = me.id
     if m.service == 'new_chat_members' and m.new_chat_members[0].id == me_id:
         await c.send_message(m.chat.id, 'Hi I can extract texts from your photo\nReply <code>/extract</code> to your photos to see it!')
+
+@app.on_message(filters.group & filters.command('extract'))
+async def extract(c: Client, m:Message):
+    try :
+        file = await c.download_media(m, in_memory=True)
+    except Exception as e :
+        print(f'error : {e}')
+        await m.reply('can\'t download file')
+    buffer = file.getbuffer()
+    try :
+        text = convert(Image.open(buffer))
+    except Exception as e :
+        print(f'error : {e}')
+        await m.reply('an error occurred, please try again or report to the admin')
